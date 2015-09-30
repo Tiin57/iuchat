6295a3b28e471c841eb7f4cef86616922ddecb2b
Alex Hicks <aldahick@iupui.edu>
2015-09-30 05:53:13 -0400

    Add security notice and work on bans.

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
+/*
+*** SECURITY NOTICE ***
+Understandably, there may be concerns about password security in an application
+that authenticates against an LDAP server. Please rest assured that any and all
+passwords that this program utilizes (aside from the LDAP credentials in config.json)
+are not stored or saved in any way except in-memory, temporarily, to authenticate users.
+Any method that accesses user passwords (not including config.json passwords, as those
+cannot be entered by users) are labeled with "*** PASSWORD ACCESS ***" for convenience.
+If you don't believe my statement, that's fine. Look through those methods yourself.
+If you don't believe that I labeled every password-accessing method, that's fine. I
+very well could have missed one. Look through all of the source code and submit a pull
+request adding the missing notice as necessary. There's a reason this is open-source,
+people.
+
+Author: Alexander David Hicks (Tiin57, aldahick)
+Date created: 29 September, 2015
+*/
 var ActiveDirectory = require("activedirectory");
 var cfg = require("./config.json");
 var bunyan = require("bunyan");
 var io = require("socket.io")(cfg.port);
 var fs = require("fs");
-var banned = require("./banned.json");
-
+var banned = {};
+fs.access("./banned.json", fs.F_OK | fs.R_OK | fs.W_OK, function(err) {
+	if (!err) {
+		banned = require("./banned.json");
+	} else {
+		fs.writeFileSync("./banned.json", "{}");
+	}
+});
 var log = bunyan.createLogger({
 	"name": "IU Chat",
 	"streams": [{
@@ -20,6 +43,20 @@ var adConfig = {
 	password: cfg.ldap.password,
 	scope: "one",
 	"log": log
+};
+
+var commands = {
+	"ban": {
+		adminOnly: true,
+		callback: function(client, args) {
+			if (args.length === 0) {
+				sendSystemMessage(client.socket, "Usage: /ban <username>");
+				return;
+			}
+			banUser(client.username, args[0]);
+			sendSystemMessage(client.socket, "Banned " + client.username);
+		}
+	}
 };
 
 function info(data) {
@@ -39,7 +76,7 @@ function getToday() {
 	return (date.getMonth() + 1).toString() + "/" + date.getDate().toString() + "/" + date.getFullYear().toString();
 }
 
-function getTime() {
+function getNow() {
 	var date = new Date();
 	return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
@@ -59,12 +96,56 @@ function sendChatMessage(socket, username, message) {
 		"username": username,
 		"message": message,
 		"date": getToday(),
-		"time": getTime(),
+		"time": getNow(),
 		"channel": "#general"
 	};
 	socket.emit("chatmsg", data);
 }
 
+function updateBans() {
+	fs.writeSync("./banned.json", JSON.stringify(banned));
+}
+
+function banUser(author, username) {
+	if (!banned[username]) {
+		banned[username] = {
+			time: getNow(),
+			date: getToday(),
+			author: author,
+			current: true,
+			pastBans: []
+		};
+	} else {
+		banned[username].time = getNow();
+		banned[username].date = getToday();
+		banned[username].author = author;
+		banned[username].current = true;
+	}
+	for (var i in clients) {
+		if (clients[i].username == username) {
+			clients[i].socket.disconnect();
+			clients.splice(i, 1);
+		}
+	}
+	updateBans();
+}
+
+function unbanUser(username) {
+	if (!banned[username]) {
+		return;
+	}
+	var ban = banned[username];
+	ban.pastBans = undefined;
+	ban.current = false;
+	banned[username].current = false;
+	banned[username].pastBans.push(ban);
+	updateBans();
+}
+
+/*
+*** PASSWORD ACCESS ***
+Checks <username> and <password> against Active Directory as a user.
+*/
 function verifyLDAP(username, password, callback) {
 	username = "ADS\\" + username;
 	var ad = new ActiveDirectory(adConfig);
@@ -89,17 +170,23 @@ function setupUserData(ad, client, callback) {
 			callback(false);
 			return;
 		}
-		console.log(user);
 		client.firstName = user.givenName;
+		if (cfg.admins[client.username]) {
+			client.isAdmin = true;
+		}
 		callback(true);
 	});
 }
 
+/*
+*** PASSWORD ACCESS *** - Specifically, the socket.on("login") callback.
+*/
 function Client(socket) {
 	this.username = "";
 	this.isLoggedIn = false;
 	this.firstName = "";
 	this.socket = socket;
+	this.isAdmin = false;
 	socket.on("login", createCallback(function(client, data) {
 		if (!data || !data.username || !data.password) {
 			invalid(data);
@@ -146,8 +233,23 @@ function Client(socket) {
 			sendSystemMessage(socket, "You are not logged in. Please refresh your page.");
 			return;
 		}
-		sendChatMessage(client.socket.broadcast, client.username, data.message);
-		sendChatMessage(client.socket, client.firstName, data.message);
+		if (data.message.startsWith("/")) {
+			var msg = data.message.substring(1);
+			var tokens = msg.split(" ");
+			var cmd = tokens.splice(0, 1).toLowerCase();
+			if (commands[cmd]) {
+				if (commands[cmd].adminOnly && !client.isAdmin) {
+					sendSystemMessage(client.socket, "You must be a chat administrator to run that command.");
+				} else {
+					(commands[cmd].callback)(client, tokens);
+				}
+			} else {
+				sendSystemMessage(client.socket, "Command " + cmd + " is not valid.");
+			}
+		} else {
+			sendChatMessage(client.socket.broadcast, client.username, data.message);
+			sendChatMessage(client.socket, client.firstName, data.message);
+		}
 	}, this));
 }
 
